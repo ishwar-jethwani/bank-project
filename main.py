bc48b9293a1749715f4656be3f982e05ff4b7e7f
ishwar <ishwar@zoconut.com>
2022-09-16 10:13:19 +0530

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from curses.ascii import isalpha, isdigit
 import random
 import json
 class BankAccount:
@@ -5,26 +6,50 @@ class BankAccount:
         self.name = name
         self.adhar_no = aadharcard
         self.pan_no = pancard
-        self.address = self.address
-        self.photo = self.photo
+        self.address = address
+        self.photo = photo
 
     def open_account(self):
         account_no = random.randint(10000000000,99999999999)
+        self.account_no = account_no
         data_list = []
-        with open("data.json","r+") as file:
+        with open("data.json","r") as file:
             data = file.read()
-            data = json.loads(data)
-            data_list.extend(data)
-            
+            if len(data)>0:
+                data = json.loads(data)
+            else:
+                data = []
+        with open("data.json","w") as file:
+            data_dict = {
+                "name": self.name,
+                "pancard_no": self.pan_no,
+                "adhar_no": self.adhar_no,
+                "address": self.address,
+                "account_no": self.account_no,
+                "profile_img":self.photo,
+                "balance": 0
+            }
+            data_list.append(data_dict)
+            data.extend(data_list)
+            data = json.dumps(data)
+            file.write(data)
+    
 
-            
-            
-
-
-
-
-
-
-class Execute:
+name = str(input("name of account holder:"))
+adhar_card = str(input("enter the adhar no:"))
+if len(adhar_card)==12 and adhar_card.isdigit()==True:
     pass
+else:
+    print("please inter the coreect no")
+pancard_no = str(input("Pancard Card No:"))
+
+a = "APGPJ1234O"
+if pancard_no[:2].isalpha()==True and pancard_no[3] in ["A","B","C","F","G","H","L","J","P","T"] and pancard_no[5:-1].isdigit()==True and pancard_no[-1].isalpha()==True:
+    pass
+else:
+    print("please write correct pan card no.")
+
+
+obj = BankAccount("ishwar","123456789","Smaya011","akjjbdfbjh","https://randomuser.me/api/portraits/thumb/men/0.jpg")
+obj.open_account()
 
